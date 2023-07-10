bb7f18a0e3bf26e9bd296fb9b060cc20b318ed37
deniandriancode <deniandriancode@gmail.com>
2023-07-10 13:28:26 +0700

    init

---
+++ src/main/java/app/LaughCounterApplication.java
@@ -0,0 +1,13 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaughCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaughCounterApplication.class, args);
+	}
+
+}
